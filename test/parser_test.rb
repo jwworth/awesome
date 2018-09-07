f4e750b2e914a5578b9ac01c7b80127d140d52c9
Jake Worth <jake@jakeworth.com>
2018-09-06 20:40:22 -0500

    Ignore rubocop's changes

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -1,23 +1,23 @@
-# frozen_string_literal: true
-
 require 'test_helper'
 require 'parser/parser'
 
 class ParserTest < Test::Unit::TestCase
+
   def test_method_with_param
     code = <<-CODE
   def method(a, b):
     true
-    CODE
+CODE
 
     nodes = Nodes.new([
-                        DefNode.new(
-                          'method',
-                          %w[a b],
-                          Nodes.new([TrueNode.new])
-                        )
-                      ])
+      DefNode.new(
+        'method',
+        ['a', 'b'],
+        Nodes.new([TrueNode.new])
+      )
+    ])
 
     assert_equal nodes, Parser.new.parse(code)
   end
+
 end
