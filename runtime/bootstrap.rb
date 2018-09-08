13ca78c9b457eff82aba9cf031994cad31fddbb0
Jake Worth <jake@jakeworth.com>
2018-09-07 19:44:16 -0500

    Add runtime

---
+++ runtime/bootstrap.rb
@@ -0,0 +1,35 @@
+require_relative 'class'
+require_relative 'context'
+
+Constants = {}
+
+Constants['Class'] = AwesomeClass.new
+Constants['Class'].runtime_class = Constants['Class']
+Constants['Object'] = AwesomeClass.new
+Constants['Number'] = AwesomeClass.new
+Constants['String'] = AwesomeClass.new
+
+root_self = Constants['Object'].new
+RootContext = Context.new(root_self)
+
+Constants['TrueClass']  = AwesomeClass.new
+Constants['FalseClass'] = AwesomeClass.new
+Constants['NilClass']   = AwesomeClass.new
+
+Constants['true']  = Constants['TrueClass'].new_with_value(true)
+Constants['false'] = Constants['FalseClass'].new_with_value(false)
+Constants['nil']   = Constants['NilClass'].new_with_value(nil)
+
+Constants['Class'].def :new do |receiver, _arguments|
+  receiver.new
+end
+
+Constants['Object'].def :print do |_receiver, arguments|
+  puts arguments.first.ruby_value
+  Constants['nil']
+end
+
+Constants['Number'].def :+ do |receiver, arguments|
+  result = arguments.reduce(0) { |sum, arg| sum + arg.ruby_value }
+  Constants['Number'].new_with_value(receiver.ruby_value + result)
+end
---
+++ runtime/context.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Context
+  attr_reader :locals, :current_self, :current_class
+  def initialize(current_self, current_class = current_self.runtime_class)
+    @locals = {}
+    @current_self = current_self
+    @current_class = current_class
+  end
+end
